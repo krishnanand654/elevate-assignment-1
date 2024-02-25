b83c7707732854a36eebd6218b05be52f4abb989
Krishnanand A <krishnanand654@gmail.com>
2024-02-25 18:54:51 +0530

    Added Exception Handling

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 def run(n):
-    x = 10/n
-    print("this is test1")
-    print("this is test2")
-    print("this is test3")
-    return x
-
+    try:
+        x = 10/n
+        return x
+    except ZeroDivisionError:
+        print(f'Error:{ZeroDivisionError}\n Cannot perform Division by zero')
+        return 0
+    finally:
+        print("this is test1")
+        print("this is test2")
+        print("this is test3")
+    
 print(run(10))  
 
